f2c23f9550aa2ada203138843339cc9399ee17a7
FermionicChaos <z3r0caffein3@gmail.com>
2025-10-27 10:40:21 -0500

    forgot these files

---
+++ inc/geodesy/phys.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef GEODESY_PHYS_H
+#define GEODESY_PHYS_H
+
+#include "phys/mesh.h"
+#include "phys/animation.h"
+#include "phys/force.h"
+#include "phys/node.h"
+
+#endif // !GEODESY_PHYS_H
